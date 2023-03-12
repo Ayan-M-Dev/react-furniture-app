64e680db7f88322995b1ab420659155d4ee9a072
Ayan Masood <ayan.m.dev@gmail.com>
2023-03-12 10:57:18 +0530

    IndexPage.js

---
+++ furniture-app/pages/IndexPage.js
@@ -0,0 +1,36 @@
+import { SafeAreaView, View, StyleSheet, Text, ScrollView, Image, TouchableOpacity, StatusBar } from "react-native";
+import { data } from "../Content";
+
+const IndexPage = ({navigation}) => {
+  return (
+    <View style={{flex:1, marginTop: StatusBar.currentHeight}}>
+      {/* Header */}
+      <View style={styles.header}>
+        <SafeAreaView>
+          <Text style={styles.headerText}>Stackie's Furniture</Text>
+          <Text style={{...styles.headerText, fontSize:15}}>The best furniture in the world</Text>
+        </SafeAreaView>
+      </View>
+      {/* Scrollview */}
+      <ScrollView style={{flexGrow:0, flexShrink:1}}>
+        {data.map((item, index) => {
+          return (
+            <TouchableOpacity 
+              key={index} 
+              style={styles.container}
+              onPress={() => navigation.navigate("DetailPage", {item})}
+            >
+              <Image style={styles.image} source={item.image} />
+              <View style={styles.innerImage}>
+                <Text style={styles.text}>{item.name}</Text>
+                <Text style={{...styles.text, color:"gray"}}>${addCommas(item.price)}</Text>
+              </View>
+            </TouchableOpacity>
+          )
+        })}
+      </ScrollView>
+    </View>
+  );
+}
+ 
+export default IndexPage;
